3a29a0d8a9e7afdca3d38f197309677e4310e975
dthemg <david.montgomery@live.se>
2019-01-25 01:13:59 +0100

    let's try this again

---
+++ translate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import chess as ch
+import os
+
+def serialize(pgn_game):
+    pass
+
+
+
+def parse_data():
+    for file in os.listdir("data"):
+        print(file)
+
+
+if __name__ == "__main__":
+    parse_games()
+
+
